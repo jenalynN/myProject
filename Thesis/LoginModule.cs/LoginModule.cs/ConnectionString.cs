20f6ba0c63ccb1aa70e0cd9ad5a888263b2e8070
jurixcahimat <42741103+jurixcahimat@users.noreply.github.com>
2018-09-05 00:18:43 +0800

    add ConnectionString.cs

---
+++ Thesis/LoginModule.cs/LoginModule.cs/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LoginModule.cs
+{
+    public static class ConnectionString
+    {
+        public const string myConnection = "Server=localhost;Database=db_poshconceptstorefinal;Uid=root;Password=";
+        
+    }
+}
