9c3c30b4c2de4204d85dd01dee55970822d6aeb1
iamryanjie <42728513+iamryanjie@users.noreply.github.com>
2018-09-23 00:56:37 -0700

    updated data chart

--- Thesis/FinalProject/dataChart.php
+++ Thesis/FinalProject/dataChart.php
@@ -6,7 +6,7 @@ require ('Controller/db.php');
 //query to get data from the table
 $query = sprintf("SELECT col_dateofpurchase as date,col_totalprice as sales FROM tbl_transaction GROUP by col_dateofpurchase ORDER BY col_dateofpurchase ASC");
 if(isset($_SESSION['userId']) && $_SESSION['usertype'] != '1'){
-	$query = sprintf("SELECT t.col_dateofpurchase as date,t.col_totalprice as sales FROM tbl_transaction t
+	$query = sprintf("SELECT t.col_dateofpurchase as date,sum(o.col_subtotal) as sales FROM tbl_transaction t
 	inner join tbl_order o on o.col_transactionid = t.col_transactionid 
 	inner join tbl_product p on o.col_productid = p.col_productid 
 	inner join tbl_brandpartner b on b.col_useraccountsid = p.col_useraccountsid 
@@ -19,7 +19,7 @@ if ( isset( $_POST['submit'] ) )
 {
 	$fromdate  = $_POST["fromdate"];
 	$todate = $_POST["todate"];
-	$query = sprintf("SELECT col_dateofpurchase as date,col_totalprice as sales 
+	$query = sprintf("SELECT col_dateofpurchase as date,sum(o.col_subtotal) as sales 
 	FROM tbl_transaction 
 	WHERE date(col_dateofpurchase) between date('$fromdate') and date('$todate')
 	GROUP by col_dateofpurchase ORDER BY col_dateofpurchase ASC");
@@ -27,7 +27,7 @@ if ( isset( $_POST['submit'] ) )
 	//echo "from: " . $fromdate . "to: " . $todate;
 	
 	if(isset($_SESSION['userId']) && $_SESSION['usertype'] != '1'){
-	$query = sprintf("SELECT t.col_dateofpurchase as date,t.col_totalprice as sales FROM tbl_transaction t
+	$query = sprintf("SELECT t.col_dateofpurchase as date,t.sum(o.col_subtotal) as sales FROM tbl_transaction t
 	inner join tbl_order o on o.col_transactionid = t.col_transactionid 
 	inner join tbl_product p on o.col_productid = p.col_productid 
 	inner join tbl_brandpartner b on b.col_useraccountsid = p.col_useraccountsid 
